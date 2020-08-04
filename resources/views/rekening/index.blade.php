46b5859dc698919bd130121cd69bf6f8b5152239
dianmustika <mustikacare@gmail.com>
2020-08-04 16:25:23 +0700

    modal

--- resources/views/rekening/index.blade.php
+++ resources/views/rekening/index.blade.php
@@ -25,7 +25,9 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Bordered Table</h3>
+                            <a href="" class="btn bg-gradient-success btn-sm">Tambah</a><button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-lg">
+                  Launch Extra Large Modal
+                </button>
                         </div>    
                     <div class="card-body">
                         <div class="card-body table-responsive p-0">
@@ -45,9 +47,9 @@
 			<td>{{ $p->norek }}</td>
 			<td>{{ $p->bank }}</td>
 			<td>
-				<a href="/rekening/edit/{{ $p->idrek }}">Edit</a>
+				<a href="/rekening/edit/{{ $p->idrek }}" class="btn bg-gradient-success btn-sm">Edit</a>
 				|
-				<a href="/rekening/hapus/{{ $p->idrek }}">Hapus</a>
+				<a href="/rekening/hapus/{{ $p->idrek }}" class="btn bg-gradient-danger btn-sm">Hapus</a>
 			</td>
 		</tr>
 		@endforeach
@@ -59,3 +61,97 @@
         </div>
     </section>
 @endsection
+
+
+
+
+
+
+
+<div class="modal fade" id="modal-xl">
+        <div class="modal-dialog modal-xl">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Extra Large Modal</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>One fine body&hellip;</p>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+      <!-- /.modal -->
+
+
+
+      <div class="modal fade" id="modal-lg">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Large Modal</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              
+
+
+ <!-- form start -->
+              <form role="form">
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputFile">File input</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                      </div>
+                      <div class="input-group-append">
+                        <span class="input-group-text" id="">Upload</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
+            </div>
+            <!-- /.card -->
+
+
+            
+
+
+            </div>
+           
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+      <!-- /.modal -->
